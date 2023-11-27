904369b8d7e19fdc8d314853b8727ffe832df7cc
Noel <noel.forsstromeriksson@skola.taby.se>
2023-11-27 09:13:42 +0100

    efh

--- Äventyrsspel.py
+++ Äventyrsspel.py
@@ -94,33 +94,48 @@ def backpack(pack):
 def level(player1):
     if player1.xp < 20:
         player1.level = 1
+        levelup()
     elif 20 <= player1.xp < 40:
         player1.level = 2
+        levelup()
         print(f"You leveled up. You are level {player1.level}")
     elif 40 <= player1.xp < 60:
         player1.level = 3
+        levelup()
         print(f"You leveled up. You are level {player1.level}")
     elif 60 <= player1.xp < 80:
         player1.level = 4
+        levelup()
         print(f"You leveled up. You are level {player1.level}")
     elif 80 <= player1.xp < 100:
         player1.level = 5
+        levelup()
         print(f"You leveled up. You are level {player1.level}")
     elif 100 <= player1.xp < 120:
         player1.level = 6
+        levelup()
         print(f"You leveled up. You are level {player1.level}")
     elif 120 <= player1.xp < 140:
         player1.level = 7
+        levelup()
         print(f"You leveled up. You are level {player1.level}")
     elif 140 <= player1.xp < 160:
         player1.level = 8
+        levelup()
         print(f"You leveled up. You are level {player1.level}")
     elif 180 <= player1.xp < 200:
         player1.level = 9
+        levelup()
         print(f"You leveled up. You are level {player1.level}")
     elif 200 <= player1.xp < 220:
         player1.level = 10
+        levelup()
         print(f"You leveled up. You are level {player1.level}")
+
+def levelup(player1):
+    if player1.level >= 2:
+        player1.strength += 100
+        player1.health += 100
 
 def battle(monster1, player1):
     print(f'You encounterd a \n--{monster1.monster}--\nstrength: {monster1.monster_strength}\nHealth: {monster1.monster_health}')
