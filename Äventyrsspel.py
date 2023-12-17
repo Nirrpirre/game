cfe7270be8773295be743017edc21e9a9438bd5a
fabbe435 <fabian.wallin@skola.taby.se>
2023-12-18 00:59:03 +0100

    a

--- Äventyrsspel.py
+++ Äventyrsspel.py
@@ -148,7 +148,7 @@ def backpack(pack):
 Your backpack is empty"""
     else:
         return pack
-
+#
 def level_up(player1):
     xp_required = player1.level * 10
     if player1.xp >= xp_required:
@@ -159,7 +159,6 @@ def level_up(player1):
         print(f"You leveled up to level {player1.level}!\n")
         
 def battle(monster1, player1, pack):
-    time.sleep(1)
     while monster1.monster_health > 0 and player1.hp > 0:
         print(f'''
 ________________________________________________________
